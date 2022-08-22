47c454db5fa1ad95dc6fa0d449de0fc4634ed42c
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-08-21 19:20:25 -0600

    Implemented CloseReservationTask.run() to close a reservation if time has lapsed and ticket not purchased. Tests pass.

--- Application/src/main/java/com/kenzie/unit/four/ticketsystem/service/CloseReservationTask.java
+++ Application/src/main/java/com/kenzie/unit/four/ticketsystem/service/CloseReservationTask.java
@@ -1,7 +1,10 @@
 package com.kenzie.unit.four.ticketsystem.service;
 
+import com.kenzie.unit.four.ticketsystem.repositories.model.ReserveTicketRecord;
 import com.kenzie.unit.four.ticketsystem.service.model.ReservedTicket;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class CloseReservationTask implements Runnable {
@@ -21,5 +24,18 @@ public class CloseReservationTask implements Runnable {
     @Override
     public void run() {
        // Your code here
+        ReservedTicket reservedTicket = reservedTicketService.findByReserveTicketId(reservedTicketsQueue.poll().getTicketId());
+        LocalDateTime timeOfReservation = LocalDateTime.parse(reservedTicket.getDateOfReservation());
+        LocalDateTime timeNow = LocalDateTime.now();
+        Duration timeLapsed = Duration.between(timeOfReservation, timeNow);
+
+        if (!reservedTicket.getTicketPurchased() && timeLapsed.getSeconds() > durationToPay) {
+            ReservedTicket closedTicket = new ReservedTicket(reservedTicket.getConcertId(),
+                    reservedTicket.getTicketId(),
+                    reservedTicket.getDateOfReservation(), true, timeNow.toString(), false);
+            reservedTicketService.updateReserveTicket(closedTicket);
+        } else {
+            reservedTicketsQueue.add(reservedTicket);
+        }
     }
 }
